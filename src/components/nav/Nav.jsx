761e5830b8577940a644970c3099222067ccde75
sanderscody26 <sanderscody26@gmail.com>
2022-05-14 13:43:10 -0400

    Added Navbar Styling and Readability
    -Added active states to navbar to allow for highlighting current state
    -Added styling to navbar, it now is fixed and readable

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,15 +5,17 @@ import {RiChatSmile3Line} from 'react-icons/ri'
 import {VscPerson} from 'react-icons/vsc'
 import {VscCode} from 'react-icons/vsc'
 import {AiOutlineAppstore} from 'react-icons/ai'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href='#'><AiOutlineHome /></a>
-      <a href='#about'><VscPerson /></a>
-      <a href='#experience'><VscCode /></a>
-      <a href='#projects'><AiOutlineAppstore /></a>
-      <a href='#contact'><RiChatSmile3Line /></a>
+      <a href='#' onClick={() => setActiveNav('#')} className={activNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activNav === '#about' ? 'active' : ''}><VscPerson /></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activNav === '#experience' ? 'active' : ''}><VscCode /></a>
+      <a href='#projects' onClick={() => setActiveNav('#projects')} className={activNav === '#projects' ? 'active' : ''}><AiOutlineAppstore /></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activNav === '#contact' ? 'active' : ''}><RiChatSmile3Line /></a>
     </nav>
   )
 }
